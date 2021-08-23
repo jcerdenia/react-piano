2fde3cc67ff944d3b52d5c190d8914801da8d8ba
Joshua Cerdenia <joshcerdenia@gmail.com>
2021-08-23 13:07:24 +0800

    Add and render Logo and Footer components

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,21 @@
+import styles from "./Logo.module.css"
+
+const Logo = () => {
+  return (
+    <h1 className={styles.logo}>
+      <span role="img" aria-label="metal hand emoji">
+        🤘
+      </span>
+      <span role="img" aria-label="musical keyboard emoji">
+        🎹
+      </span>
+      <span role="img" aria-label="musical notes emoji">
+        🎶
+      </span>
+    </h1>
+  )
+}
+
+// NB role="img" attribute helps screen readers to correctly parse the content.
+
+export default Logo;
