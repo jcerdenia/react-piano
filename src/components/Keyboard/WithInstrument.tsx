41008967419ff072356842755ce0613e81e47c84
Joshua Cerdenia <joshcerdenia@gmail.com>
2021-08-26 14:41:58 +0800

    Enable dynamically loading instruments with InstrumentSelector

--- src/components/Keyboard/WithInstrument.tsx
+++ src/components/Keyboard/WithInstrument.tsx
@@ -1,11 +1,20 @@
+import { useEffect } from "react"
+import { useInstrument } from "../../state/Instrument/Context"
 import { useAudioContext } from "../AudioContextProvider"
 import { useSoundfont } from "../../adapters/Soundfont/useSoundfont"
-import { useMount } from "../../utils/useMount"
 import { Keyboard } from "./Keyboard"
 
 export const KeyboardWithInstrument = () => {
   const AudioContext = useAudioContext()!
-  const { loading, play, stop, load } = useSoundfont({ AudioContext })
-  useMount(load)
+  const { instrument } = useInstrument()
+  const { loading, current, play, stop, load } = useSoundfont({ AudioContext })
+
+  useEffect(() => {
+    // This useEffect hook replaces useMount(load) which was here earlier since
+    // we want to dynamically change our instrument's sound set, rather than
+    // loading it once when mounted.
+    if (!loading && instrument !== current) load(instrument)
+  }, [load, loading, current, instrument])
+
   return <Keyboard loading={loading} play={play} stop={stop} />
 }
--- src/components/Playground/Playground.tsx
+++ src/components/Playground/Playground.tsx
@@ -1,13 +1,14 @@
 import { InstrumentContextProvider } from "../../state/Instrument/Provider"
 import { InstrumentSelector } from "../InstrumentSelector"
 import { KeyboardWithInstrument } from "../Keyboard"
+import styles from "./Playground.module.css"
 
 export const Playground = () => {
   // Here we wrap our components with InstrumentContextProvider
   // to provide access to InstrumentContext.
   return (
     <InstrumentContextProvider>
-      <div className="playground">
+      <div className={styles.playground}>
         <KeyboardWithInstrument />
         <InstrumentSelector />
       </div>
