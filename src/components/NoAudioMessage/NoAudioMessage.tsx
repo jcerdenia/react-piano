2149f83d6f3d0fcf5984ddf9eca7c02edd560143
Joshua Cerdenia <joshcerdenia@gmail.com>
2021-08-26 10:43:11 +0800

    Change emoji

--- src/components/NoAudioMessage/NoAudioMessage.tsx
+++ src/components/NoAudioMessage/NoAudioMessage.tsx
@@ -1,7 +1,7 @@
 export const NoAudioMessage = () => {
   return (
     <div>
-      <p>Sorry, it's not gonna work. 😢</p>
+      <p>Sorry, it's not gonna work. 😭</p>
       <p>Looks like your browser doesn't support <code>Audio API</code>.</p>
     </div>
   )
